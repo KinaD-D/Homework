52e9997704e3f9ccfc8bcd3ba5783dcfb80bd347
KinaD-D <72291273+KinaD-D@users.noreply.github.com>
2020-11-28 10:47:34 +0300

    Add files via upload

---
+++ Урок 28.11.2020/Урок 28.11.2020/Поиск.py
@@ -0,0 +1,10 @@
+x = input('Введдите символы, а все что между /* и */ исключат, вводить границы можно несколько раз')
+while x.find('/*') != -1 or x.find('*/') != -1:
+	finder1 = x.find('/*')
+	finder2 = x.find('*/')
+	deliter = x[finder1: finder2 + 2]
+	x = x.replace(deliter, '')
+	print('Убрано', deliter)
+	print('Осталось', x)
+print('\n\n\nВ результате получилось', x)
+input()
